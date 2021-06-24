3a4a65b815ce5db3ddb5e560d158931964afdaa2
Erlend Dahl <erlend.dahl@gmail.com>
2021-06-24 09:15:01 +0200

    Implemented a spiral enumerator to enumerate the grid in a spiral instead of in a right-to-left/top-down manner, better utilizing the sequential line performance gain.

---
+++ LambdaModel.Tests/Utilities/SpiralGridEnumeratorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using LambdaModel.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LambdaModel.Tests.Utilities
+{
+    [TestClass]
+    public class SpiralGridEnumeratorTests
+    {
+        [TestMethod]
+        public void TinyTest()
+        {
+            var values = SpiralGridEnumerator.Enumerate(1).ToArray();
+            Assert.AreEqual(9, values.Length);
+
+            var correct = new (int x, int y)[]
+            {
+                (-1, -1),
+                (0, -1),
+                (1, -1),
+                (1, 0),
+                (1, 1),
+                (0, 1),
+                (-1, 1),
+                (-1, 0),
+                (0, 0)
+            };
+
+            for (var i = 0; i < correct.Length; i++)
+            {
+                Assert.AreEqual(correct[i].x, values[i].x);
+                Assert.AreEqual(correct[i].y, values[i].y);
+            }
+        }
+
+        [TestMethod]
+        public void LargerTest()
+        {
+            var values = SpiralGridEnumerator.Enumerate(10).ToArray();
+
+            // Correct array size
+            Assert.AreEqual((10 * 2 + 1) * (10 * 2 + 1), values.Length);
+
+            // All values within valid range
+            foreach (var v in values)
+            {
+                Assert.IsTrue(v.x >= -10);
+                Assert.IsTrue(v.x <= 10);
+                Assert.IsTrue(v.y >= -10);
+                Assert.IsTrue(v.y <= 10);
+            }
+
+            // No duplicates
+            Assert.IsTrue(values.GroupBy(p => p.x + "_" + p.y).All(p => p.Count() == 1));
+        }
+    }
+}
---
+++ LambdaModel/Utilities/SpiralGridEnumerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaModel.Utilities
+{
+    public static class SpiralGridEnumerator
+    {
+        /// <summary>
+        /// Returns coordinates in a grid going in a clockwise spiral starting in the upper left corner, ending at the center.
+        /// For example, given a radius of 2, it returns the coordinates in this sequence:
+        ///
+        ///     1  2  3  4  5
+        ///     16 17 18 19 6
+        ///     15 24 25 20 7
+        ///     14 23 22 21 8
+        ///     13 12 11 10 9
+        /// 
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public static IEnumerable<(int x, int y)> Enumerate(int radius)
+        {
+            var minY = -radius;
+            var maxY = radius;
+            var minX = -radius;
+            var maxX = radius;
+            while (maxY >= minY || maxX >= minX)
+            {
+                for (var x = minX; x <= maxX; x++)
+                    yield return (x, minY);
+                minY++;
+
+                for (var y = minY; y <= maxY; y++)
+                    yield return (maxX, y);
+                maxX--;
+
+                for (var x = maxX; x >= minX; x--)
+                    yield return (x, maxY);
+                maxY--;
+
+                for (var y = maxY; y >= minY; y--)
+                    yield return (minX, y);
+                minX++;
+            }
+        }
+    }
+}
