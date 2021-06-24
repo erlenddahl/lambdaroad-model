027b63f489741283d87c1bdb35412e11964b6bf4
Erlend Dahl <erlend.dahl@gmail.com>
2021-06-24 10:09:43 +0200

    Commented code.

--- LambdaModel/Grid/GridCalculator.cs
+++ LambdaModel/Grid/GridCalculator.cs
@@ -25,6 +25,8 @@ namespace LambdaModel.Grid
 
             center.Z = tiles.GetAltitude(center);
 
+            // Initialize a PointUtm array that is to be (re)used as the vector of points from
+            // the center to each of the points that should be calculated.
             _vector = new PointUtm[(int)Math.Sqrt(radius * radius * 2) + 1];
             for (var i = 0; i < _vector.Length; i++)
                 _vector[i] = new PointUtm(0, 0);
@@ -33,17 +35,36 @@ namespace LambdaModel.Grid
             _calc = new PathLossCalculator();
         }
 
+        /// <summary>
+        /// Calculates path loss for all points along the line from the center to the given coordinates.
+        /// Skips any points along the line that has already been calculated.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>The number of new calculations that were made.</returns>
         public int CalculateTo(int x, int y)
         {
+            // Get the X,Y vector from the center to these coordinates.
             var vectorLength = Tiles.FillVector(_vector, Center.X, Center.Y, Center.X + x, Center.Y + y);
             var calculations = 0;
+
             for (var i = 2; i < vectorLength; i++)
             {
+                // Get the current receiver point
                 var c = _vector[i];
+
+                // Calculate the location of the results for this point in the results matrix
                 var (xi, yi) = ((int)(c.X - Center.X) + Radius, (int)(c.Y - Center.Y) + Radius);
+
+                // If this point has already been calculated, get out of here.
                 if (Results[xi, yi] != 0) continue;
+
+                // Otherwise, make sure the Z values in the vector are retrieved up until this point.
                 Tiles.FillAltitudeVector(_vector, i);
+
+                // Calculate the loss for this point, and store it in the resuls matrix
                 Results[xi, yi] = _calc.CalculateLoss(_vector, 100, 2, i - 1);
+
                 calculations++;
             }
 
