65c7c97e89ad9cf12b273ac5655204db9e8c5a84
Erlend Dahl <erlend.dahl@gmail.com>
2021-06-24 14:27:33 +0200

    Created class for reading a road network shape file, generating a high performance cache file, and enumerating this cache to find nearby road links.

---
+++ LambdaModel.Tests/FullRun/RoadNetwork/FindLinksTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LambdaModel.General;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LambdaModel.Tests.FullRun.RoadNetwork
+{
+    [TestClass]
+    public class FindLinksTests
+    {
+        [TestMethod]
+        public void NothingHere()
+        {
+            var links = ShapeLink.ReadLinks(@"..\..\..\..\Data\RoadNetwork\2021-05-28_smaller.shp", new PointUtm(0, 0), 100).ToArray();
+            Assert.AreEqual(0, links.Length);
+        }
+
+        [TestMethod]
+        public void OneHere()
+        {
+            var links = ShapeLink.ReadLinks(@"..\..\..\..\Data\RoadNetwork\2021-05-28_smaller.shp", new PointUtm(275007.95, 7042725.97), 50).ToArray();
+            Assert.AreEqual(1, links.Length);
+        }
+
+        [TestMethod]
+        public void ALotHere()
+        {
+            var links = ShapeLink.ReadLinks(@"..\..\..\..\Data\RoadNetwork\2021-05-28_smaller.shp", new PointUtm(288608.1, 7033525.3), 1000).ToArray();
+            Assert.AreEqual(42, links.Length);
+        }
+
+        [TestMethod]
+        public void MoreHere()
+        {
+            var links = ShapeLink.ReadLinks(@"..\..\..\..\Data\RoadNetwork\2021-05-28_smaller.shp", new PointUtm(271868.3, 7041337.0), 5_000).ToArray();
+            Assert.AreEqual(8492, links.Length);
+        }
+    }
+}
---
+++ LambdaModel/General/ShapeReader.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DotSpatial.Data;
+using DotSpatial.Topology;
+
+namespace LambdaModel.General
+{
+    public class ShapeLink
+    {
+        public int ID { get; }
+        public PointUtm[] Geometry { get; }
+
+        private ShapeLink(int id, IEnumerable<PointUtm> geometry)
+        {
+            ID = id;
+            Geometry = geometry.ToArray();
+        }
+
+        /// <summary>
+        /// Reads all links that are within the given radius from the center point.
+        /// </summary>
+        /// <param name="geometryPath"></param>
+        /// <param name="center"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public static IEnumerable<ShapeLink> ReadLinks(string geometryPath, PointUtm center, double radius)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var cc = new Coordinate(center.X, center.Y);
+            if (!File.Exists(geometryPath + ".cache"))
+                GenerateShapeCache(geometryPath);
+
+            using (var reader = new BinaryReader(File.OpenRead(geometryPath + ".cache")))
+            {
+                var fileSize = reader.BaseStream.Length;
+                var position = 0;
+
+                while (position < fileSize)
+                {
+                    var ix = reader.ReadInt32();
+
+                    var dx = reader.ReadInt32() - cc.X;
+                    var dy = reader.ReadInt32() - cc.Y;
+                    var length = reader.ReadInt32();
+
+                    var name = reader.ReadString();
+
+                    var pointCount = reader.ReadInt32();
+
+                    var pointBytes = reader.ReadBytes(pointCount * 24);
+                    position += 4 + 4 * 3 + 4 + name.Length + 4 + pointBytes.Length;
+
+                    if (Math.Sqrt(dx * dx + dy * dy) - length > radius)
+                        continue;
+
+                    var geometry = new PointUtm[pointCount];
+                    var pix = 0;
+                    var anyInside = false;
+                    for (var i = 0; i < pointBytes.Length; i += 24)
+                    {
+                        var x = BitConverter.ToDouble(pointBytes, i);
+                        var y = BitConverter.ToDouble(pointBytes, i + 8);
+                        var z = BitConverter.ToDouble(pointBytes, i + 16);
+                        geometry[pix++] = new PointUtm(x, y, z);
+
+                        if (!anyInside)
+                        {
+                            dx = x - cc.X;
+                            dy = y - cc.Y;
+                            if (Math.Sqrt(dx * dx + dy * dy) <= radius)
+                                anyInside = true;
+                        }
+                    }
+
+                    if (anyInside)
+                        yield return new ShapeLink(ix, geometry);
+                }
+            }
+        }
+
+        private static void GenerateShapeCache(string geometryPath)
+        {
+            using(var writer = new BinaryWriter(File.Create(geometryPath + ".cache")))
+            using (var fs = FeatureSet.Open(geometryPath))
+            {
+                //Note: using foreach() on fs.Features loads the entire file to memory. Slow and OOM.
+                for (var i = 0; i < fs.NumRows(); i++)
+                {
+                    writer.Write(i);
+
+                    var feature = fs.GetFeature(i);
+                    // To read more features, do this: x = (int)feature.DataRow["A"];
+
+                    var ec = feature.BasicGeometry.Envelope.Center();
+                    writer.Write((int)ec.X);
+                    writer.Write((int)ec.Y);
+                    writer.Write((int)CalculateLength(feature.BasicGeometry.Coordinates));
+
+                    var id = $"{feature.DataRow["FROM_M"]}-{feature.DataRow["TO_M"]}@{feature.DataRow["ROUTEID"]}";
+                    writer.Write(id);
+
+                    writer.Write(feature.BasicGeometry.Coordinates.Count);
+
+                    foreach (var c in feature.BasicGeometry.Coordinates)
+                    {
+                        writer.Write(c.X);
+                        writer.Write(c.Y);
+                        writer.Write(c.Z);
+                    }
+                }
+            }
+        }
+
+        private static double CalculateLength(IList<Coordinate> coordinates)
+        {
+            var l = 0d;
+            for (var i = 1; i < coordinates.Count; i++)
+                l += coordinates[i - 1].Distance(coordinates[i]);
+            return l;
+        }
+    }
+}
