2e62eaf4c13e47fbfdccaaf56b4bb6a12293cc20
Erlend Dahl <erlend.dahl@gmail.com>
2021-06-25 14:41:26 +0200

    Suppress TIFF parsing warnings.

--- LambdaModelRunner/Program.cs
+++ LambdaModelRunner/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using BitMiracle.LibTiff.Classic;
 using ConsoleUtilities.ConsoleInfoPanel;
 using LambdaModel.Calculations;
 using LambdaModel.Terrain;
+using LambdaModel.Terrain.Tiff;
 using no.sintef.SpeedModule.Geometry.SimpleStructures;
 
 namespace LambdaModelRunner
@@ -26,6 +28,8 @@ namespace LambdaModelRunner
 
             using (var cip = new ConsoleInformationPanel("Running road network signal loss calculations"))
             {
+                Tiff.SetErrorHandler(new LambdaTiffErrorHandler(cip));
+
                 var center = new Point3D(271327, 7040324);
                 var radius = 50_000;
                 var tileSize = 512;
@@ -47,6 +51,51 @@ namespace LambdaModelRunner
                 road.SaveResults(@"..\..\..\..\Data\RoadNetwork\test-results-huge.shp");
                 cip.Set("Saving time", $"{DateTime.Now.Subtract(start).TotalSeconds:n2} seconds.");
             }
+        }
+    }
+
+    internal class LambdaTiffErrorHandler : TiffErrorHandler
+    {
+        private readonly ConsoleInformationPanel _cip;
+
+        private AppendableStringInfoItem _log = null;
+
+        public LambdaTiffErrorHandler(ConsoleInformationPanel cip)
+        {
+            _cip = cip;
+        }
+
+        private void Log(string text)
+        {
+            return;
+            if (_log == null)
+            {
+                _log = _cip.Log("Tiff parsing", text, sequence: 999);
+            }
+            else
+            {
+                _log.AppendLine(text);
+            }
+        }
+
+        public override void ErrorHandler(Tiff tif, string method, string format, params object[] args)
+        {
+            //Log("ERROR: " + string.Format(format, args) + " (" + method + ", " + System.IO.Path.GetFileName(tif.FileName()) + ")");
+        }
+
+        public override void ErrorHandlerExt(Tiff tif, object clientData, string method, string format, params object[] args)
+        {
+            Log("ERROR: " + string.Format(format, args) + " (" + method + ", " + System.IO.Path.GetFileName(tif.FileName()) + ")");
+        }
+
+        public override void WarningHandler(Tiff tif, string method, string format, params object[] args)
+        {
+            //Log("WARNING: " + string.Format(format, args) + " (" + method + ", " + System.IO.Path.GetFileName(tif.FileName()) + ")");
+        }
+
+        public override void WarningHandlerExt(Tiff tif, object clientData, string method, string format, params object[] args)
+        {
+            Log("WARNING: " + string.Format(format, args) + " (" + method + ", " + System.IO.Path.GetFileName(tif.FileName()) + ")");
         }
     }
 }
