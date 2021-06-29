7ba64f29e8cb282953dd11d909cf494cdb2aa539
Erlend Dahl <erlend.dahl@gmail.com>
2021-06-29 14:08:14 +0200

    More fields, less properties.

--- LambdaModel/General/Point4D.cs
+++ LambdaModel/General/Point4D.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LambdaModel.Utilities;
 using no.sintef.SpeedModule.Geometry.SimpleStructures;
 
 namespace LambdaModel.General
 {
     public class Point4D : Point3D
     {
-        public double M { get; set; }
+        public double M;
+        public int RoundedX;
+        public int RoundedY;
 
         public Point4D(double x, double y, double z = 0, double m = 0) : base(x, y, z)
         {
